5a60a94be471349f4855cbec4026520094441cad
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-01 12:09:04 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit
+      React ⚛️ + Vite ⚡ + Replit!
+      <br/>
+      <h4 className="bg-teal-500">Vielleicht klappt's diesmal!</h4>
     </main>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base:'/friendly-system/',
+  plugins: [react(),tailwindcss()],
   server: {
     host: '0.0.0.0',
   }
