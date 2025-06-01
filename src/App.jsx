9aab08e0acec4a5c5bc3a91fd64c3ea796887858
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-01 12:58:33 +0000

    added ThemeToggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import {ThemeToggle} from "./components/ThemeToggle";
 import { StarBackground } from "./components/StarBackground";
+
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit!
-      <br/>
-      <h4 className="bg-teal-500">Vielleicht klappt's diesmal!</h4>
-      <StarBackground/>
-    </main>
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground /></div>
   )
 }
