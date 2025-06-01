a1e70eda13415408d437d088f560d7dec1cfc645
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-01 12:16:12 +0000

    added star background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-
+import { StarBackground } from "./components/StarBackground";
 export default function App() {
   return (
     <main>
       React ⚛️ + Vite ⚡ + Replit!
       <br/>
       <h4 className="bg-teal-500">Vielleicht klappt's diesmal!</h4>
+      <StarBackground/>
     </main>
   )
 }
